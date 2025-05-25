cb982affed35b30d576520ef3f8478593e64796d
AbrahamAhmad05 <abrahamahmad.005@gmail.com>
2025-05-25 10:50:21 +0530

    resposive bug fixed

--- src/components/about/AboutHero.js
+++ src/components/about/AboutHero.js
@@ -5,7 +5,7 @@ const AboutHero = () => {
     return (
         <div className='bg-[url(/about_imgs/hero.png)] h-[50vh] sm:h-screen  bg-contain bg-no-repeat md:bg-cover flex justify-center items-center text-[#2E0A49]'>
             <div className="flex pt-32 md:pt-80 2xl:mt-20 px-4 text-center w-2xl">
-                <h6 className={`${gotham.className}  capitalize text-3xl font-medium `}>We're not just building a gold app. We're building a habit of wealth.</h6>
+                <h6 className={`${gotham.className}  capitalize text-3xl font-medium `}>We&apos;re not just building a gold app. We&apos;re building a habit of wealth.</h6>
             </div>
         </div>
     )
