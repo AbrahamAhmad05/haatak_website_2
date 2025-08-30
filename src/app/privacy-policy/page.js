0aade27337937221f3002976b6f18c494595f165
Abhihipparagi <hipparagiabhit@gmail.com>
2025-08-31 00:36:53 +0530

    updated privacy policy page

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -25,7 +25,7 @@ const PrivacyPolicyPage = () => {
 
                         {/* Publication date */}
                         <div className={`${montserrat.className} text-sm sm:text-base md:text-lg py-3 md:py-4 text-[#2E0A49]`}>
-                            1 Mar 2025
+                            25 August 2025
                         </div>
 
                         {/* Policy content with responsive text sizing */}
@@ -35,91 +35,142 @@ const PrivacyPolicyPage = () => {
                                 Introduction
                             </h3>
                             <p>
-                                Welcome to Haatak! We are Unibridg Assets and Vault Private Limited., located at 455 D, 1st Floor, Eshaan Arcade, REMCO BHEL Layout, Rajarajeshwari Nagar, Bengaluru - 560098. Your privacy is important to us. We deeply appreciate your trust and are committed to safeguarding your privacy.
+                                Welcome to Haatak! We are GrowYuga Fintech Private Limited., located at #558, 2nd Floor, 3rd Phase, JP Nagar, Bengaluru - 560078. Your privacy is important to us. We deeply appreciate your trust and are committed to safeguarding your privacy.
                             </p>
                             <p>
                                 This policy explains how we collect, use, and protect your personal information when you visit our website, WAP site, or mobile applications. By using our services, you agree to the terms of this Privacy Policy. If you do not agree, please do not use our website or apps. Note that we may update this policy occasionally, so please check it regularly.
                             </p>
 
-                            <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
-                                General Information
-                            </h3>
-                            <ul className="list-disc pl-5 space-y-2">
-                                <li><span className="font-semibold">No Selling or Renting:</span> We do not sell, share, or rent your personal information to third parties. We will not use your email address or phone number for unsolicited messages.</li>
-                                <li><span className="font-semibold">Statistical Data:</span> We may share general statistics about our Customers, such as visitor numbers or purchase types, but this information will not personally identify you.</li>
-                                <li><span className="font-semibold">Legal Requests:</span> We may disclose your personal information if required by law or if a legal request complies with regulations.</li>
-                            </ul>
 
-                            <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
-                                What We Collect
-                            </h3>
-                            <ul className="list-disc pl-5 space-y-2">
-                                <li><span className="font-semibold">Personal Information:</span> This includes your name, address, phone number, email, payment details, and any other information you provide when using our services.</li>
-                                <li><span className="font-semibold">Technical Data:</span> We may collect information about your internet connection, IP address, and browsing behavior.</li>
-                            </ul>
+                            {/* Sourabh Code Start */}
+                                <div className="  pl-4">
+                                    <ol className="list-decimal pl-5 space-y-3 text-[#2E0A49]">
+                                        <div className='introduction'>
+                                            <li>
+                                                 <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
+                                            General Information:
+                                        </h3>
+                                         <p className='text-[#000000]' >
+                                                1.1 <b>No Selling or Renting:</b> We do not sell, share, or rent your personal information to third parties. We will not use your email address or phone number for unsolicited messages.
 
-                            <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
-                                How We Use Your Information
-                            </h3>
-                            <ul className="list-disc pl-5 space-y-2">
-                                <li><span className="font-semibold">Service Provision:</span> To fulfill your requests for our products and services.</li>
-                                <li><span className="font-semibold">Communication:</span> To send you updates, offers, and information about our services.</li>
-                                <li><span className="font-semibold">Customization:</span> To tailor our services to your preferences and interests.</li>
-                                <li><span className="font-semibold">Security:</span> To detect and prevent fraud and other illegal activities.</li>
-                                <li><span className="font-semibold">Feedback:</span> Occasionally, we may ask you to participate in surveys to improve our services.</li>
-                            </ul>
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                1.2 <b>Statistical Data:</b> We may share general statistics about our Customers, such as visitor numbers or purchase types, but this information will not personally identify you.
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                1.3  <b>Legal Requests:</b> We may disclose your personal information if required by law or if a legal request complies with regulations.
+                                             </p>
+                                            </li>
+                                            <li>
+                                                <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
+                                           What We Collect:
+                                        </h3>
+                                        <p className='text-[#000000]' >
+                                                2.1 <b>Personal Information:</b> This includes your name, address, phone number, email, payment details, and any other information you provide when using our services.
 
-                            <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
-                                Cookies
-                            </h3>
-                            <ul className="list-disc pl-5 space-y-2">
-                                <li><span className="font-semibold">What Are Cookies:</span> Cookies are small files stored on your device to help us remember your preferences and improve your browsing experience.</li>
-                                <li><span className="font-semibold">Usage:</span> We use cookies to enhance your experience on our site. Cookies do not collect personal information unless you have provided it previously.</li>
-                                <li><span className="font-semibold">Third Parties:</span> We may share aggregated cookie data with third parties.</li>
-                            </ul>
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                2.2 <b>Technical Data:</b> We may collect information about your internet connection, IP address, and browsing behavior.
+                                             </p>
+                                            </li>
+                                            <li>
+                                                 <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
+                                            How We Use Your Information:
+                                        </h3>
+                                        <p className='text-[#000000]' >
+                                                3.1 <b>Service Provision:</b> To fulfill your requests for our products and services.
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                3.2 <b>Communication:</b> To send you updates, offers, and information about our services.
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                3.3 <b>Customization:</b> To tailor our services to your preferences and interests.
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                3.4 <b>Security:</b> To detect and prevent fraud and other illegal activities.
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                3.5 <b>Feedback:</b>  Occasionally, we may ask you to participate in surveys to improve our services.
+                                             </p>
+                                            </li>
+                                            <li>
+                                                <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
+                                           Cookies:
+                                        </h3>
+                                        <p className='text-[#000000]' >
+                                                4.1 <b>What Are Cookies:</b> Cookies are small files stored on your device to help us remember your preferences and improve your browsing experience.
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                4.2 <b>Usage:</b> We use cookies to enhance your experience on our site. Cookies do not collect personal information unless you have provided it previously.
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                4.3 <b>Third Parties:</b> We may share aggregated cookie data with third parties.
+                                             </p>
+                                            </li>
+                                            <li>
+                                                <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
+                                           Links to Other Websites:
+                                        </h3>
+                                        <p className='text-[#000000]'>
+                                            Our website may contain links to other sites. We are not responsible for their privacy practices or content.
+                                        </p>
+                                            </li>
+                                            <li>
+                                                 <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
+                                           Security:
+                                        </h3> 
+                                        <p className='text-[#000000]' >
+                                                6.1 <b>Protection Measures:</b> We implement strict security measures to safeguard your information from unauthorized access or misuse.
+                                             </p>
+                                             <br/>
+                                             <p className='text-[#000000]' >
+                                                6.2 <b>Secure Access:</b> When you access or update your account information, we use secure servers to ensure your data is protected.
 
-                            <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
-                                Links to Other Websites
-                            </h3>
-                            <p>
-                                Our website may contain links to other sites. We are not responsible for their privacy practices or content.
+                                             </p>   
+                                            </li>
+                                            <li>
+                                                 <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
+                                          Your Consent
+                                        </h3> 
+                                        <p className='text-[#000000]'>
+                                            By using Haatak, you consent to our collection, use, and sharing of your information as described in this Privacy Policy.
+                                        </p>
+                                        <br></br>
+                                      
+                                            </li>
+
+                                            <p className='text-[#000000]'>
+                                If you have any questions about our grievance policy terms, please contact us at our corporate office:
                             </p>
-
-                            <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
-                                Security
-                            </h3>
-                            <ul className="list-disc pl-5 space-y-2">
-                                <li><span className="font-semibold">Protection Measures:</span> We implement strict security measures to safeguard your information from unauthorized access or misuse.</li>
-                                <li><span className="font-semibold">Secure Access:</span> When you access or update your account information, we use secure servers to ensure your data is protected.</li>
-                            </ul>
-
-                            <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
-                                Your Consent
-                            </h3>
-                            <p>
-                                By using Haatak, you consent to our collection, use, and sharing of your information as described in this Privacy Policy.
-                            </p>
-
-                            <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
-                                Contact Us
-                            </h3>
-                            <p>
-                                If you have any questions about our privacy practices and terms, please contact us at our corporate office:
-                            </p>
-                            <address className="not-italic mt-3">
-                                #558, 9th Cross, 3rd Phase, J P Nagar, Bengaluru 560078
+                             <address className='text-[#000000]'>
+                            GrowYuga Fintech Private Limited<br />  
+                            Haatak  <br /> 
+                            #558, 2nd Floor, 3rd Phase, <br /> 
+                            JP Nagar, Bengaluru - 560078.<br /> 
                             </address>
-
-                            <h3 className={`${gilroy_semibold.className} text-lg md:text-xl text-[#2E0A49] mt-6 mb-2`}>
-                                Policy Changes
-                            </h3>
-                            <p>
-                                Haatak reserves the right to amend this privacy policy at any time without prior notice. Changes will be posted on our website, and it is the customer’s responsibility to review the policy regularly.
+                            <br/>
+                            <p className='text-[#000000]'>
+                            Policy Changes: Haatak reserves the right to amend this refund policy at any time without prior notice. Changes will be posted on our website, and it is the customer’s responsibility to review the policy regularly.
                             </p>
 
-                            <p className="mt-8 italic">
-                                Thank you for trusting Haatak with your information.
+                            <br/>
+
+                            <p className='text-[#000000]'>
+                            Thank you for your understanding and cooperation!                         
                             </p>
+                                        </div>
+                                    </ol>
+                                </div>
+                            {/* Sourabh Code End */}
                         </div>
                     </section>
                 </article>
